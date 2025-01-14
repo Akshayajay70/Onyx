4d97e8c975701b2d3bd81c54cc1a78014e4abe2c
Akshay A <akshayajay0724@gmail.com>
2025-01-14 20:22:27 +0530

    changed the logic for blocked user in middleware

--- middlewares/userMiddlewares.js
+++ middlewares/userMiddlewares.js
@@ -1,17 +1,44 @@
-const checkSession = (req, res, next) => {
-    if (req.session.user) {
-        next()
-    } else {
-        res.redirect('/login')
+import userModel from "../model/userModel.js"
+
+const checkSession = async (req, res, next) => {
+    try {
+        // Check if session exists
+        if (!req.session.user) {
+            return res.redirect('/login?message=Please+login+to+continue&alertType=info');
+        }
+
+        // Verify user exists and is active
+        const user = await userModel.findById(req.session.user);
+        
+        if (!user) {
+            // User no longer exists
+            req.session.destroy();
+            return res.redirect('/login?message=Account+not+found&alertType=error');
+        }
+
+        if (user.blocked) {
+            // User is blocked
+            req.session.destroy();
+            return res.redirect('/login?message=Your+account+has+been+blocked&alertType=error');
+        }
+
+        next();
+
+    } catch (error) {
+        console.error('Session Check Error:', error);
+        return res.redirect('/login?message=Session+error+occurred&alertType=error');
     }
 }
 
-const isLogin  = (req, res, next)=>{
- 
-    if(req.session.user){
-        res.redirect('/home')
-    }else{
-        next()
+const isLogin = async (req, res, next) => {
+    try {
+        if (req.session.user) {
+            return res.redirect('/home');
+        }
+        next();
+    } catch (error) {
+        console.error('Login Check Error:', error);
+        next();
     }
 }
 
